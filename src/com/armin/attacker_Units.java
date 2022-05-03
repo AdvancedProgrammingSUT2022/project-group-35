e876e6a80f6099697129d58c778bb88afccbfef4
Armin L <lohrasbi.armin@gmail.com>
2022-05-03 14:02:18 +0430

    units that can attack other enemies (range-melee-catapult)

---
+++ src/com/armin/attacker_Units.java
@@ -0,0 +1,20 @@
+package com.armin;
+
+public class attacker_Units {
+
+    private String attacker_code;
+
+    public attacker_Units(String unit_attacker_name,String unit_attacker_code1, int unit_attacker_cost, int unit_attacker_range, int unit_attacker_movement_speed, int unit_attacker_health, String unit_attacker_ability)
+    {
+        this.setName(unit_attacker_name);
+        attacker_code = unit_attacker_code1;
+        this.setCost(unit_attacker_cost);
+        this.setAttackerRange(unit_attacker_range);
+        this.setMovementSpeed(unit_attacker_movement_speed);
+        this.setAttackerHealth(unit_attacker_health);
+        this.setAttackerAbility(unit_attacker_ability);
+    }
+
+    public String getAttackerCode() {return attacker_code;}
+    public void setAttackerCode(String unit_attacker_code1) {attacker_code = unit_attacker_code1;}
+}
