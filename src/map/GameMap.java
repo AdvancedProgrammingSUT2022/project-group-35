5fa7a31ea8e5ae9067c61cc30996d6769acd51fa
Armin L <lohrasbi.armin@gmail.com>
2022-05-04 13:35:12 +0430

    Test new graphics (incomplete)

---
+++ src/map/GameMap.java
@@ -0,0 +1,6 @@
+package map;
+
+public class GameMap {
+
+    private Tile[][] tiles;
+}
---
+++ src/map/Tile.java
@@ -0,0 +1,14 @@
+package map;
+
+import gfx.SpriteLibrary;
+
+import java.awt.*;
+
+public class Tile {
+
+    private Image Spirite;
+
+    public Tile(SpirteLibrary spiriteLibrary) {
+
+    }
+}
